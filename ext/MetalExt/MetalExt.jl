31743949e9a5585c15029e3c487c7961172b760b
Philip Fackler <facklerpw@ornl.gov>
2026-08-27 12:30:35 -0500

    Update Metal maxthreads
    Update barrier call
    
    Fix Metal launch

--- ext/MetalExt/MetalExt.jl
+++ ext/MetalExt/MetalExt.jl
@@ -11,16 +11,27 @@ default_stream() = Metal.global_queue(Metal.device())
 
 JACC.default_stream(::MetalBackend) = default_stream()
 
+function JACC.create_stream(::MetalBackend)
+    Metal.MTL.MTLCommandQueue(Metal.device())
+end
+
 function JACC.synchronize(::MetalBackend)
     Metal.synchronize()
 end
 
-function dummy() end
+@inline function _make_kernel(kernel_function, kargs, ::Nothing)
+    p_tt = Tuple{Core.Typeof.(Metal.mtlconvert.((kargs)))...}
+    return Metal.mtlfunction(kernel_function, p_tt)
+end
 
-@inline function _kernel_maxthreads(N, f, x...)
-    kernel = @metal launch=false dummy()
-    max_threads = Int32(kernel.pipeline.maxTotalThreadsPerThreadgroup)
-    return max_threads
+@inline function _make_kernel(kernel_function, kargs, kname::AbstractString)
+    p_tt = Tuple{Core.Typeof.(Metal.mtlconvert.((kargs)))...}
+    return Metal.mtlfunction(kernel_function, p_tt; name = kname)
+end
+
+@inline function _kernel_maxthreads(kernel_function, kargs, kname)
+    p_kernel = _make_kernel(kernel_function, kargs, kname)
+    return (p_kernel, p_kernel.maxthreads)
 end
 
 @inline function _launch(::Nothing, threads, groups, f, args...)
@@ -32,10 +43,11 @@ end
 end
 
 function JACC.parallel_for(f, ::MetalBackend, N::Integer, x...; name = nothing)
-    max_threads_per_group = _kernel_maxthreads(N, f, x)
-    threads = min(N, max_threads_per_group)
+    kargs = (N, f, x...)
+    kernel, max_threads = _kernel_maxthreads(_parallel_for_metal, kargs, name)
+    threads = min(N, max_threads)
     groups = cld(N, threads)
-    _launch(name, threads, groups, _parallel_for_metal, N, f, x...)
+    kernel(kargs...; threads = threads, groups = groups)
     Metal.synchronize()
 end
 
@@ -49,7 +61,9 @@ function JACC.parallel_for(
     Nblocks = cld(N, threads[2])
     blocks = (Mblocks, Nblocks)
 
-    _launch(name, threads, blocks, _parallel_for_metal_MN, M, N, f, x...)
+    kargs = (M, N, f, x...)
+    kernel = _make_kernel(_parallel_for_metal_MN, kargs, name)
+    kernel(kargs...; threads = threads, groups = blocks)
     Metal.synchronize()
 end
 
@@ -65,22 +79,25 @@ function JACC.parallel_for(
     Nblocks = cld(N, threads[3])
     blocks = (Lblocks, Mblocks, Nblocks)
 
-    _launch(name, threads, blocks, _parallel_for_metal_LMN, L, M, N, f, x...)
+    kargs = (L, M, N, f, x...)
+    kernel = _make_kernel(_parallel_for_metal_LMN, kargs, name)
+    kernel(kargs...; threads = threads, groups = blocks)
     Metal.synchronize()
 end
 
 function JACC.parallel_for(
         f, spec::LaunchSpec{MetalBackend}, N::Integer, x...; name = nothing)
-    max_threads_per_group = _kernel_maxthreads(N, f, x)
+    kargs = (N, f, x...)
+    kernel, max_threads = _kernel_maxthreads(_parallel_for_metal, kargs, name)
     if spec.threads == 0
-        maxItems = max_threads_per_group
+        maxItems = max_threads
         spec.threads = min(N, maxItems)
     end
     if spec.blocks == 0
         spec.blocks = cld(N, spec.threads)
     end
 
-    _launch(name, spec.threads, spec.blocks, _parallel_for_metal, N, f, x...)
+    kernel(kargs...; threads = spec.threads, groups = spec.blocks, queue = spec.stream)
 
     if spec.sync
         Metal.synchronize()
@@ -102,8 +119,9 @@ function JACC.parallel_for(
         spec.blocks = (Mblocks, Nblocks)
     end
 
-    _launch(
-        name, spec.threads, spec.blocks, _parallel_for_metal_MN, M, N, f, x...)
+    kargs = (M, N, f, x...)
+    kernel = _make_kernel(_parallel_for_metal_MN, kargs, name)
+    kernel(kargs...; threads = spec.threads, groups = spec.blocks, queue = spec.stream)
 
     if spec.sync
         Metal.synchronize()
@@ -127,10 +145,9 @@ function JACC.parallel_for(
         spec.blocks = (Lblocks, Mblocks, Nblocks)
     end
 
-    _launch(name, spec.threads, spec.blocks,
-        _parallel_for_metal_LMN, L, M, N, f, x...)
-    @metal threads=spec.threads groups=spec.blocks _parallel_for_metal_LMN(
-        L, M, N, f, x...)
+    kargs = (L, M, N, f, x...)
+    kernel = _make_kernel(_parallel_for_metal_LMN, kargs, name)
+    kernel(kargs...; threads = spec.threads, groups = spec.blocks, queue = spec.stream)
 
     if spec.sync
         Metal.synchronize()
@@ -177,16 +194,25 @@ function JACC._parallel_reduce!(reducer::JACC.ParallelReduce{MetalBackend},
     op = reducer.op
     init = reducer.init
 
-    items = _kernel_maxthreads(N, f, x)
-    groups = cld(N, items)
-    shmem_size = items * sizeof(init)
+    kargs1 = (Val(512), N, op, wk.tmp, init, f, x...)
+    kernel1,
+    threads1 = _kernel_maxthreads(_parallel_reduce_metal, kargs1,
+        _make_kname(name, "block_reduce"))
+
+    kargs2 = (Val(512), 1, op, wk.tmp, init, wk.ret)
+    kernel2,
+    threads2 = _kernel_maxthreads(_reduce_kernel_metal, kargs2,
+        _make_kname(name, "grid_reduce"))
+
+    threads = min(threads1, threads2, 512)
+    groups = cld(N, threads)
 
     _init!(wk, groups, init)
 
-    _launch(_make_kname(name, "block_reduce"), items, groups,
-        _parallel_reduce_metal, Val(items), N, op, wk.tmp, init, f, x...)
-    _launch(_make_kname(name, "grid_reduce"), items, 1, _reduce_kernel_metal,
-        Val(items), groups, op, wk.tmp, init, wk.ret)
+    kernel1(Val(threads), N, op, wk.tmp, init, f, x...; threads = threads,
+        groups = groups, queue = reducer.stream)
+    kernel2(Val(threads), groups, op, wk.tmp, init, wk.ret; threads = threads,
+        groups = 1, queue = reducer.stream)
 
     if reducer.sync
         Metal.synchronize()
@@ -197,20 +223,34 @@ end
 
 function JACC.parallel_reduce(f, ::MetalBackend, N::Integer, x...; op, init,
         name = nothing)
-    items = _kernel_maxthreads(N, f, x)
-    groups = cld(N, items)
-    ret = fill!(Metal.MtlArray{typeof(init)}(undef, groups), init)
+    ret_inst = Metal.MtlArray{typeof(init)}(undef, 0)
+    kargs1 = (Val(512), N, op, ret_inst, init, f, x...)
+    kernel1,
+    threads1 = _kernel_maxthreads(_parallel_reduce_metal, kargs1, _make_kname(name, "block_reduce"))
+
     rret = Metal.MtlArray([init])
-    _launch(_make_kname(name, "block_reduce"), items, groups,
-        _parallel_reduce_metal, Val(items), N, op, ret, init, f, x...)
-    _launch(_make_kname(name, "grid_reduce"), items, 1, _reduce_kernel_metal,
-        Val(items), groups, op, ret, init, rret)
+    kargs2 = (Val(512), 1, op, ret_inst, init, rret)
+    kernel2,
+    threads2 = _kernel_maxthreads(_reduce_kernel_metal, kargs2, _make_kname(name, "grid_reduce"))
+
+    threads = min(threads1, threads2, 512)
+    groups = cld(N, threads)
+
+    ret = Metal.MtlArray{typeof(init)}(undef, groups)
+
+    kernel1(Val(threads), N, op, ret, init, f, x...;
+        threads = threads, groups = groups)
+    kernel2(Val(threads), groups, op, ret, init,
+        rret; threads = threads, groups = 1)
     Metal.synchronize()
+
     return Base.Array(rret)[]
 end
 
 function JACC._parallel_reduce!(reducer::JACC.ParallelReduce{MetalBackend},
         (M, N)::NTuple{2, Integer}, f, x...; name = nothing)
+    wk = reducer.workspace
+    op = reducer.op
     init = reducer.init
     numItems = 16
     Mitems = numItems
@@ -218,15 +258,17 @@ function JACC._parallel_reduce!(reducer::JACC.ParallelReduce{MetalBackend},
     threads = (Mitems, Nitems)
     Mgroups = cld(M, threads[1])
     Ngroups = cld(N, threads[2])
-    blocks = (Mgroups, Ngroups)
+    groups = (Mgroups, Ngroups)
 
-    wk = reducer.workspace
-    _init!(wk, blocks, init)
+    _init!(wk, groups, init)
 
-    _launch(_make_kname(name, "block_reduce"), threads, blocks,
-        _parallel_reduce_metal_MN, (M, N), reducer.op, wk.tmp, init, f, x...)
-    _launch(_make_kname(name, "grid_reduce"), threads, (1, 1),
-        _reduce_kernel_metal_MN, blocks, reducer.op, wk.tmp, init, wk.ret)
+    kargs1 = ((M, N), op, wk.tmp, init, f, x...)
+    kernel1 = _make_kernel(_parallel_reduce_metal_MN, kargs1, _make_kname(name, "block_reduce"))
+    kernel1(kargs1...; threads = threads, groups = groups, queue = reducer.stream)
+
+    kargs2 = (groups, op, wk.tmp, init, wk.ret)
+    kernel2 = _make_kernel(_reduce_kernel_metal_MN, kargs2, _make_kname(name, "grid_reduce"))
+    kernel2(kargs2...; threads = threads, groups = (1, 1), queue = reducer.stream)
 
     if reducer.sync
         Metal.synchronize()
@@ -244,12 +286,18 @@ function JACC.parallel_reduce(f, ::MetalBackend, (M, N)::NTuple{2, Integer},
     Mgroups = cld(M, Mitems)
     Ngroups = cld(N, Nitems)
     groups = (Mgroups, Ngroups)
-    ret = fill!(Metal.MtlArray{typeof(init)}(undef, (Mgroups, Ngroups)), init)
+
+    ret = Metal.MtlArray{typeof(init)}(undef, groups)
     rret = Metal.MtlArray([init])
-    _launch(_make_kname(name, "block_reduce"), items, groups,
-        _parallel_reduce_metal_MN, (M, N), op, ret, init, f, x...)
-    _launch(_make_kname(name, "grid_reduce"), items, (1, 1),
-        _reduce_kernel_metal_MN, groups, op, ret, init, rret)
+
+    kargs1 = ((M, N), op, ret, init, f, x...)
+    kernel1 = _make_kernel(_parallel_reduce_metal_MN, kargs1, _make_kname(name, "block_reduce"))
+    kernel1(kargs1...; threads = items, groups = groups)
+
+    kargs2 = (groups, op, ret, init, rret)
+    kernel2 = _make_kernel(_reduce_kernel_metal_MN, kargs2, _make_kname(name, "grid_reduce"))
+    kernel2(kargs2...; threads = items, groups = (1, 1))
+
     Metal.synchronize()
     return Base.Array(rret)[]
 end
@@ -277,7 +325,7 @@ function _parallel_reduce_metal(
 
     max_pwr = JACC.ilog2(shmem_length) - 1
     for p in (max_pwr:-1:0)
-        threadgroup_barrier()
+        threadgroup_barrier(Metal.MemoryFlagThreadGroup)
         tn = 2^p
         if ti <= tn
             @inbounds shared_mem[ti] = op(shared_mem[ti], shared_mem[ti + tn])
@@ -303,7 +351,7 @@ function _reduce_kernel_metal(
 
     max_pwr = JACC.ilog2(shmem_length) - 1
     for p in (max_pwr:-1:0)
-        threadgroup_barrier()
+        threadgroup_barrier(Metal.MemoryFlagThreadGroup)
         tn = 2^p
         if i <= tn
             @inbounds shared_mem[i] = op(shared_mem[i], shared_mem[i + tn])
@@ -333,7 +381,7 @@ function _parallel_reduce_metal_MN((M, N), op, ret, init, f, x...)
     end
 
     for n in (8, 4, 2, 1)
-        threadgroup_barrier()
+        threadgroup_barrier(Metal.MemoryFlagThreadGroup)
         if (ti <= n && tj <= n)
             @inbounds shared_mem[ti, tj] = op(
                 shared_mem[ti, tj], shared_mem[ti + n, tj + n])
@@ -362,7 +410,7 @@ function _reduce_kernel_metal_MN((M, N), op, red, init, ret)
     @inbounds shared_mem[i, j] = tmp
 
     for n in (8, 4, 2, 1)
-        threadgroup_barrier()
+        threadgroup_barrier(Metal.MemoryFlagThreadGroup)
         if i <= n && j <= n
             @inbounds shared_mem[i, j] = op(
                 shared_mem[i, j], shared_mem[i + n, j + n])
@@ -416,11 +464,11 @@ end
 
 function JACC.shared(::MetalBackend, x::AbstractArray)
     shmem = Metal.mtl(x; storage = Metal.SharedStorage)
-    # Metal.threadgroup_barrier()
+    # Metal.threadgroup_barrier(Metal.MemoryFlagThreadGroup)
     return shmem
 end
 
-JACC.sync_workgroup(::MetalBackend) = Metal.threadgroup_barrier()
+JACC.sync_workgroup(::MetalBackend) = Metal.threadgroup_barrier(Metal.MemoryFlagThreadGroup)
 
 JACC.array_type(::MetalBackend) = Metal.MtlArray
 
